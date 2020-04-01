d4b14224e799940a4c4b2a5c9ea9f75f4e30abb6
Aditya <aditya@gumgum.com>
2020-04-01 00:15:45 -0700

    njit removed

--- al/ranking_of_scores.py
+++ al/ranking_of_scores.py
@@ -1,24 +1,22 @@
 import numpy as np
-from numba import njit, prange
 
 # Uncertainty Sampling
-@njit
 def bordas_sample_score(ranking_matrix):
-	U_s, q = ranking_matrix.shape()
+	U_s = np.shape(ranking_matrix)[0]
+	q = np.shape(ranking_matrix)[-1]
 	scores = np.sum((U_s - ranking_matrix), axis = -1) / (q*( U_s - 1))
 	return scores
-@njit
+
 def get_ranking(inp_array):
 	temp = inp_array.argsort()
 	ranks = np.empty_like(temp)
 	ranks[temp] = np.arange(len(inp_array))
 	return ranks
 
-@njit(parallel = True)
 def uncertainty_sampling(prediction_matrix):
 	margin_matrix = np.absolute((2*prediction_matrix) - 1)
 	ranking_matrix = np.zeros(np.shape(margin_matrix))
-	for i in prange(margin_matrix.shape()[-1]):
+	for i in range(np.shape(margin_matrix)[-1]):
 		ranking_matrix[:, i] = get_ranking(margin_matrix[:, i])
 	sample_scores = bordas_sample_score(ranking_matrix)
 
